692a44e1a51a03e540028a4733bf14c668410397
Aidan Grimshaw <grimshawaidan@gmail.com>
2020-05-31 14:45:11 -0700

    having different localsize variables

--- submit.bash
+++ submit.bash
@@ -7,7 +7,7 @@
 #SBATCH -e first.err
 #SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH --mail-user=grimshaa@oregonstate.edu
-for LOCAL_SIZE in 8 64 128 256 512
+for LOCAL_SIZE in 64
 do
     g++ -D LOCAL_SIZE=$LOCAL_SIZE -o first first.cpp /usr/local/apps/cuda/cuda-10.1/lib64/libOpenCL.so.1.1 -lm -fopenmp
     ./first
