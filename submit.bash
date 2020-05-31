10094bfc51c750d465bf27ee7bdd2c8e3727783b
Aidan Grimshaw <grimshawaidan@gmail.com>
2020-05-31 02:06:33 -0700

    modified submit.bash

--- submit.bash
+++ submit.bash
@@ -1,11 +1,11 @@
 #!/bin/bash
-#SBATCH -J printinfo
+#SBATCH -J first
 #SBATCH -A cs475-575
 #SBATCH -p class
 #SBATCH --gres=gpu:1
-#SBATCH -o printinfo.out
-#SBATCH -e printinfo.err
+#SBATCH -o first.out
+#SBATCH -e first.err
 #SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH --mail-user=grimshaa@oregonstate.edu
-g++ -o printinfo printinfo.cpp /usr/local/apps/cuda/cuda-10.1/lib64/libOpenCL.so.1.1 -lm -fopenmp
-./printinfo
+g++ -o first first.cpp /usr/local/apps/cuda/cuda-10.1/lib64/libOpenCL.so.1.1 -lm -fopenmp
+./first
