50367eef1bb1bf825a127886de19b1ba63dd3a9d
Aidan Grimshaw <grimshawaidan@gmail.com>
2020-05-31 15:32:48 -0700

    having different num elements

--- submit.bash
+++ submit.bash
@@ -9,9 +9,10 @@
 #SBATCH --mail-user=grimshaa@oregonstate.edu
 
 for NUM_ELEMENTS in 1000 50000 250000 1000000 8000000
+do
     for LOCAL_SIZE in 8 64 128 256 512
     do
-        g++ -DLOCAL_SIZE=$LOCAL_SIZE -o first first.cpp /usr/local/apps/cuda/cuda-10.1/lib64/libOpenCL.so.1.1 -lm -fopenmp -w
+        g++ -DLOCAL_SIZE=$LOCAL_SIZE -DNUM_ELEMENTS=$NUM_ELEMENTS -o first first.cpp /usr/local/apps/cuda/cuda-10.1/lib64/libOpenCL.so.1.1 -lm -fopenmp -w
         ./first
     done
 done
